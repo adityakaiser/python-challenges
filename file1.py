9e4405c003800c8df334c52199bcc4aecb38e2b6
adityakaiser <singhadityakaiser@gmail.com>
2026-04-01 20:45:36 +0530

    Add files via upload

---
+++ file1.py
@@ -0,0 +1,30 @@
+a=20
+b=30
+c=10
+d=1
+z=25
+w=(a+b)*z/a
+print(w,"is  the answer")
+
+
+
+# 4) Store a name in `name` and a number in `age`.
+
+# 5) Check this condition using `or` and `and`:
+
+# - The code checks if `name` is "Alex"
+
+# OR (if `name` is "John" AND `age` is 2 or more).
+
+# - If the condition is true, print the welcome message.
+
+# - Otherwise, print the goodbye message.
+
+
+name= 'JOJO'
+age=  12
+if name=='JOJO' or 'kakyoin' and  age<=12:
+    print("hello")
+else:
+    print("goodbye")
+
